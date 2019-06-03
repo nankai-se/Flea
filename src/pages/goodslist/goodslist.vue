503b34e17b86d5fb1a805245fcc6adc44f19f37c
Dio <lwind2013@163.com>
2019-06-03 21:55:43 +0800

    fix:conflict.

---
+++ src/pages/goodslist/goodslist.vue
@@ -0,0 +1,39 @@
+<template>
+  <div @click="handle">
+    <div class="search">
+      <search></search>
+    </div>
+    <div class="itempanel">
+      <itempanel></itempanel>
+    </div>
+  </div>
+</template>
+
+<script>
+import itempanel from '../../components/itempanel.vue'
+import search from '../../components/search.vue'
+
+export default {
+  name: 'goodslist',
+  components: {
+    itempanel,
+    search
+  },
+  data () {
+    return {
+      imageURL: '//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg'
+    }
+  },
+  methods: {
+    handle () {
+      console.log('ddd')
+    }
+  }
+}
+</script>
+
+<style>
+.search {
+  padding-bottom:15%
+}
+</style>
